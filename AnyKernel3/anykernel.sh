2d3bba002e9112cda13ace5c763b7100e528f4d0
Akari <akariondev@gmail.com>
2024-09-08 18:52:53 -0300

    AK3: switch device to bangkk

--- AnyKernel3/anykernel.sh
+++ AnyKernel3/anykernel.sh
@@ -4,13 +4,13 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Mjolnir-Kernel
+kernel.string=MoeKernel
 do.devicecheck=1
 do.modules=1
 do.systemless=0
 do.cleanup=1
 do.cleanuponabort=0
-device.name1=fogos
+device.name1=bangkk
 supported.versions=14.0
 supported.patchlevels=
 '; } # end properties
