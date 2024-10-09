6680c14686fba1319b4145bd2a24986c0e5a24d2
simonpunk <16465163+simonpunk@users.noreply.github.com>
2024-10-09 17:11:40 +0800

    ksu/fs/include/kernel: import susfs4ksu 1.4.2 to k5.4
    * Patch: 50_add_susfs_in_kernel-5.4.patch
    * Patch: 10_enable_susfs_for_ksu.patch
    * fs/include: susfs: fixup header paths - fs/susfs.c - include/linux/sus_su.h
    * drivers: staging: ksu: Disable susfs path hiding
    * drivers: staging: ksu: Kconfig: Disable KSU_SUSFS_SUS_SU (Log: https://telegra.ph/Disable-KSU-SUSFS-SUS-SU-11-10)
    * ref: https://gitlab.com/simonpunk/susfs4ksu/-/tree/1.4.2-kernel-5.4?ref_type=tags
    
    Signed-off-by: TogoFire <togofire@mailfence.com>
    Change-Id: 095b721e0d74d64cfcf438e9ab88c60342cf3b44
    Co-Authored-By: TogoFire <togofire@mailfence.com>

--- fs/devpts/inode.c
+++ fs/devpts/inode.c
@@ -596,7 +596,8 @@ struct dentry *devpts_pty_new(struct pts_fs_info *fsi, int index, void *priv)
 	return dentry;
 }
 
-#ifdef CONFIG_KSU
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+extern bool ksu_devpts_hook;
 extern int ksu_handle_devpts(struct inode*);
 #endif
 
@@ -608,9 +609,12 @@ extern int ksu_handle_devpts(struct inode*);
  */
 void *devpts_get_priv(struct dentry *dentry)
 {
-	#ifdef CONFIG_KSU
-	ksu_handle_devpts(dentry->d_inode);
-	#endif
+#ifdef CONFIG_KSU_SUSFS_SUS_SU
+	if (likely(ksu_devpts_hook)) {
+		ksu_handle_devpts(dentry->d_inode);
+	}
+#endif
+
 	if (dentry->d_sb->s_magic != DEVPTS_SUPER_MAGIC)
 		return NULL;
 	return dentry->d_fsdata;
